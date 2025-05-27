786d34303e7ac44a6647a2514f2a483fdddadf14
BT_1N3_06 <g@gmacl.comn>
2025-05-27 10:16:11 -0400

    second commit

---
+++ src/Player.java
@@ -0,0 +1,133 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Player {
+    private final int MOVE_AMT = 3;
+    private BufferedImage right;
+    private boolean facingRight;
+    private int xCoord;
+    private int yCoord;
+    private int score;
+    private Animation animation;
+
+    public Player() {
+        facingRight = true;
+        xCoord = 50; // starting position is (50, 435), right on top of ground
+        yCoord = 435;
+        score = 0;
+        try {
+            right = ImageIO.read(new File("src\\marioright.png"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //The code below is used to create an ArrayList of BufferedImages to use for an Animation object
+        //By creating all the BufferedImages beforehand, we don't have to worry about lagging trying to read image files during gameplay
+        ArrayList<BufferedImage> images = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            String filename = "src\\tile00" + i + ".png";
+            try {
+                images.add(ImageIO.read(new File(filename)));
+            }
+            catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        animation = new Animation(images,50);
+    }
+
+    //This function is changed from the previous version to let the player turn left and right
+    //This version of the function, when combined with getWidth() and getHeight()
+    //Allow the player to turn without needing separate images for left and right
+    public int getxCoord() {
+        if (facingRight) {
+            return xCoord;
+        } else {
+            return (xCoord + (getPlayerImage().getWidth()));
+        }
+    }
+
+    public int getyCoord() {
+        return yCoord;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    //These functions are newly added to let the player turn left and right
+    //These functions when combined with the updated getxCoord()
+    //Allow the player to turn without needing separate images for left and right
+    public int getHeight() {
+        return getPlayerImage().getHeight();
+    }
+
+    public int getWidth() {
+        if (facingRight) {
+            return getPlayerImage().getWidth();
+        } else {
+            return getPlayerImage().getWidth() * -1;
+        }
+    }
+
+    public void faceRight() {
+        facingRight = true;
+    }
+
+    public void faceLeft() {
+        facingRight = false;
+    }
+
+    // newly added, used for right-clicking to turn
+    public void turn() {
+        if (facingRight) {
+            faceLeft();
+        } else {
+            faceRight();
+        }
+    }
+
+    public void moveRight() {
+        if (xCoord + MOVE_AMT <= 920) {
+            xCoord += MOVE_AMT;
+        }
+    }
+
+    public void moveLeft() {
+        if (xCoord - MOVE_AMT >= 0) {
+            xCoord -= MOVE_AMT;
+        }
+    }
+
+    public void moveUp() {
+        if (yCoord - MOVE_AMT >= 0) {
+            yCoord -= MOVE_AMT;
+        }
+    }
+
+    public void moveDown() {
+        if (yCoord + MOVE_AMT <= 435) {
+            yCoord += MOVE_AMT;
+        }
+    }
+
+    public void collectCoin() {
+        score++;
+    }
+
+    public BufferedImage getPlayerImage() {
+        return animation.getActiveFrame();  // updated
+    }
+
+    // we use a "bounding Rectangle" for detecting collision
+    public Rectangle playerRect() {
+        int imageHeight = getPlayerImage().getHeight();
+        int imageWidth = getPlayerImage().getWidth();
+        Rectangle rect = new Rectangle(xCoord, yCoord, imageWidth, imageHeight);
+        return rect;
+    }
+}
